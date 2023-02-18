a6c6d7f2f2a47265ea35b166b1e6b63cdfafe99d
howecofe <am0229@naver.com>
2023-02-18 19:30:26 +0900

    2-3. 서비스 레이어: 비즈니스 로직

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,15 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class TodoController {
-    //testTodo 메서드 작성하기
-    @GetMapping
+
+    @Autowired
+    private TodoService service;
+    @GetMapping("/test")
     public ResponseEntity<?> testTodo() {
+        String str = service.testService();
         List<String> list = new ArrayList<>();
-        list.add("Hello World! And you got 500 error.");
+        list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-        return ResponseEntity.internalServerError().body(response);
+        return ResponseEntity.ok().body(response);
     }
 }
