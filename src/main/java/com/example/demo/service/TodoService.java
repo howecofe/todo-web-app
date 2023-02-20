6c126d09b6306caaac1759bc75685cc1bbd636d3
howecofe <am0229@naver.com>
2023-02-20 19:45:21 +0900

    2-8. 서비스 개발: Delete Todo 구현

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -74,4 +74,22 @@ public class TodoService {
         // 5. Retrieve Todo에서 만든 메서드를 이용해 유저의 모든 Todo 리스트를 리턴한다.
         return retrieve(entity.getUserId());
     }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        // 1. 저장할 엔티티가 유효한지 확인한다.
+        validate(entity);
+
+        try {
+            // 2. 엔티티를 삭제한다.
+            repository.delete(entity);
+        } catch (Exception e) {
+            // 3. exception 발생 시 id와 exception을 로깅한다.
+            log.error("error deleting entity ", entity.getId(), e);
+
+            // 4. 컨트롤러로 exception을 날린다. 데이터베이스 내부 로직을 캡슐화하기 위해 e를 리턴하지 않고 새 exception 오브젝트를 리턴한다.
+            throw new RuntimeException("error deleting entity " + entity.getId());
+        }
+        // 5. 새 Todo 리스트를 가져와 리턴한다.
+        return retrieve(entity.getUserId());
+    }
 }
